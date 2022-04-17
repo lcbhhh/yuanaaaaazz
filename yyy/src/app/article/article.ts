0c5d7d022482c4550f2e35a870711d317d9f4760
lcbhhh <103880874+lcbhhh@users.noreply.github.com>
2022-04-17 23:08:02 +0800

    Add files via upload

---
+++ yyy/src/app/article/article.ts
@@ -0,0 +1,35 @@
+import { isNull } from "@angular/compiler/src/output/output_ast";
+
+export class Article{
+    title:string;
+    link:string;
+    votes:number;
+    
+    constructor(title:string,link:string,votes?:number) {
+      this.title =title;
+      this.link =link;
+      this.votes =votes || 0;
+     }
+
+     voteUp(){
+         this.votes++;
+    
+     }
+
+     voteDown(){
+         this.votes--;
+         if(this.votes<=0){
+             this.votes =0;
+         }
+     }
+   
+     domain(): string{
+         try{
+             const link:string=this.link.split('//')[1];
+             return link.split('/')[0];
+         }catch (err) {
+             return null as any;
+         }
+     }
+
+}
